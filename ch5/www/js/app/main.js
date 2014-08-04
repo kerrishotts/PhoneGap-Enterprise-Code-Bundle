23ad2d7052f1952817f06acdd3adb69fc8bfebe9
Kerri Shotts <kerrishotts@gmail.com>
2014-08-04 01:13:39 -0500

    ch5 app

---
+++ ch5/www/js/app/main.js
@@ -0,0 +1,54 @@
+/**
+ *
+ * main.js
+ * @author Kerri Shotts
+ * @version 3.0.0
+ *
+ * Copyright (c) 2014 Packt Publishing
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following
+ * conditions:
+ * The above copyright notice and this permission notice shall be included in all copies
+ * or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+/*jshint
+         asi:true,
+         bitwise:true,
+         browser:true,
+         curly:true,
+         eqeqeq:false,
+         forin:true,
+         noarg:true,
+         noempty:true,
+         plusplus:false,
+         smarttabs:true,
+         sub:true,
+         trailing:false,
+         undef:true,
+         white:false,
+         onevar:false 
+ */
+/*global define*/
+define( [ "yasmf", "app/lib/rest" ], function( _y, REST ) {
+  // define our app object
+  var APP = {};
+  APP.start = function() {
+    REST.baseURI = "https://localhost:4445";
+    REST.post( "/auth", {
+      "userId": "JDOE",
+      "candidatePassword": "password"
+    }).then ( function (r) {
+      console.log (r);
+    });
+  };
+  return APP;
+} );
