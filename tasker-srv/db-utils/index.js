1bc3507b14a6b4cc0e6471eace136824a6aeebfd
Kerri Shotts <kerrishotts@gmail.com>
2014-08-07 01:05:46 -0500

    try/catch if the connection is down

--- tasker-srv/db-utils/index.js
+++ tasker-srv/db-utils/index.js
@@ -31,28 +31,44 @@ DBUtils.prototype.query = function ( sql, bindParameters, cb) {
 			winston.error ( " Failed to acquire connection from the pool." );
 			cb (new Error (err));
 		}
-		client.execute ( sql, bindParameters, function ( err, results ) {
-			if (err) {
-				clientPool.release ( client);
-				cb (new Error (err));
-			}
-			clientPool.release ( client );
-			cb (err, results );
-		});
+    try
+    {
+      client.execute(sql, bindParameters, function (err, results)
+      {
+        if (err)
+        {
+          clientPool.release(client);
+          cb(new Error(err));
+        }
+        clientPool.release(client);
+        cb(err, results);
+      });
+    }
+    catch (err) {
+      try
+      {
+        clientPool.release(client);
+      }
+      catch (err2)
+      {
+        // can't do anything...
+      }
+      cb(err);
+    }
 	});
 };
 
 DBUtils.prototype.execute = DBUtils.prototype.query;
 
-DBUtils.prototype.outInteger = function (options) { return new oracle.OutParam ( oracle.OCCIINT, options ); }
-DBUtils.prototype.outVarchar2 = function (options) { return new oracle.OutParam ( oracle.OCCISTRING, options ); }
-DBUtils.prototype.outDouble = function (options) { return new oracle.OutParam ( oracle.OCCIDOUBLE, options ); }
-DBUtils.prototype.outFloat = function (options) { return new oracle.OutParam ( oracle.OCCIFLOAT, options ); }
-DBUtils.prototype.outCursor = function (options) { return new oracle.OutParam ( oracle.OCCICURSOR, options ); }
-DBUtils.prototype.outClob = function (options) { return new oracle.OutParam ( oracle.OCCICLOB, options ); }
-DBUtils.prototype.outDate = function (options) { return new oracle.OutParam ( oracle.OCCIDATE, options ); }
-DBUtils.prototype.outTimestamp = function (options) { return new oracle.OutParam ( oracle.OCCITIMESTAMP, options ); }
-DBUtils.prototype.outBlob = function (options) { return new oracle.OutParam ( oracle.OCCIBLOB, options ); }
-DBUtils.prototype.outNumber = function (options) { return new oracle.OutParam ( oracle.OCCINUMBER, options ); }
+DBUtils.prototype.outInteger = function (options) { return new oracle.OutParam ( oracle.OCCIINT, options ); };
+DBUtils.prototype.outVarchar2 = function (options) { return new oracle.OutParam ( oracle.OCCISTRING, options ); };
+DBUtils.prototype.outDouble = function (options) { return new oracle.OutParam ( oracle.OCCIDOUBLE, options ); };
+DBUtils.prototype.outFloat = function (options) { return new oracle.OutParam ( oracle.OCCIFLOAT, options ); };
+DBUtils.prototype.outCursor = function (options) { return new oracle.OutParam ( oracle.OCCICURSOR, options ); };
+DBUtils.prototype.outClob = function (options) { return new oracle.OutParam ( oracle.OCCICLOB, options ); };
+DBUtils.prototype.outDate = function (options) { return new oracle.OutParam ( oracle.OCCIDATE, options ); };
+DBUtils.prototype.outTimestamp = function (options) { return new oracle.OutParam ( oracle.OCCITIMESTAMP, options ); };
+DBUtils.prototype.outBlob = function (options) { return new oracle.OutParam ( oracle.OCCIBLOB, options ); };
+DBUtils.prototype.outNumber = function (options) { return new oracle.OutParam ( oracle.OCCINUMBER, options ); };
 
 module.exports = DBUtils;
