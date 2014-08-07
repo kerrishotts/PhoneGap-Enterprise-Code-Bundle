ee9b79d593e14d5283b5248c5db66476cd15735b
Kerri Shotts <kerrishotts@gmail.com>
2014-08-07 01:03:47 -0500

    better word - intermediate

--- ch5/www/js/app/models/session.js
+++ ch5/www/js/app/models/session.js
@@ -4,13 +4,13 @@ define ( [ "app/lib/objUtils", "../lib/cryptojs" ], function ( objUtils, CryptoJ
       this.sessionId = data.sessionId;
       this.userId = data.userId;
       this.sessionSalt = data.sessionSalt;
-      this.nextIncompleteToken = data.nextToken;
+      this.nextIntermediateToken = data.nextIntermediateToken;
     }
   }
 
   Session.prototype.computeNextToken = function () {
-    var token1 = this.nextIncompleteToken.substr(0,256);
-    var token2 = this.nextIncompleteToken.substr(256,256);
+    var token1 = this.nextIntermediateToken.substr(0,256);
+    var token2 = this.nextIntermediateToken.substr(256,256);
     var token2H = CryptoJS.PBKDF2( token2, this.sessionSalt,
                                    {keySize: 32, iterations: 256}).toString(CryptoJS.enc.Hex).toUpperCase();
     return token1+token2H;
