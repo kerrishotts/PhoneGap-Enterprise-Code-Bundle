ba7f79a49687b9e2c199ca01ac59799b9d672c75
Kerri Shotts <kerrishotts@gmail.com>
2014-09-28 01:28:30 -0500

    get tasks; require hmac

--- tasker-srv/api-def/task/getTaskList.js
+++ tasker-srv/api-def/task/getTaskList.js
@@ -29,77 +29,92 @@
 // dependencies
 //
 var apiUtils = require( "../../api-utils" ),
+  security = require( "../security" ),
+  Errors = require( "../../errors" ),
+  Task = require( "../../models/task" ),
+  getTaskAction = require( "./getTask" ),
+  DBUtils = require( "../../db-utils" ),
 
   getTaskListAction = {
-    "title": "Tasks",
-    "action": "get-tasks",
-    "verb": "get",
-    "securedBy": "tasker-auth",
-    "description": "Returns the tasks that the authenticated user is authorized to see.",
-    "href": "/tasks",
-    "accepts": [ "application/hal+json", "application/json", "text/json" ],
-    "sends": [ "application/hal+json", "application/json", "text/json" ],
-    "queryParameters": {
-      "owned-by": {
-        "title": "Owner",
-        "key": "ownedBy",
-        "type": "integer",
-        "required": false
-      },
-      "assigned-to": {
-        "title": "Assigned to",
-        "key": "assignedTo",
-        "type": "integer",
-        "required": false
-      },
-      "with-status": {
-        "title": "Status",
-        "key": "withStatus",
-        "type": "string",
-        "required": false,
-        "enum": [ {
-          title: "In Progress",
-          value: "I"
-        }, {
-          title: "On Hold",
-          value: "H"
-        }, {
-          title: "Complete",
-          value: "C"
-        }, {
-          title: "Deleted",
-          value: "X"
-        } ]
+    "title":            "Tasks",
+    "action":           "get-tasks",
+    "verb":             "get",
+    "secured-by":       "tasker-auth",
+    "hmac":             "tasker-256",
+    "description":      "Returns the tasks that the authenticated user is authorized to see.",
+    "href":             "/tasks",
+    "base-href":        "/tasks",
+    "accepts":          [ "application/hal+json", "application/json", "text/json" ],
+    "sends":            [ "application/hal+json", "application/json", "text/json" ],
+    "query-parameters": {
+      "owned-by":           { "title": "Owner", "key": "ownedBy", "type": "integer", "required": false },
+      "assigned-to":        { "title": "Assigned to", "key": "assignedTo", "type": "integer", "required": false },
+      "with-status":        { "title": "Status", "key": "withStatus", "type": "string", "required": false,
+        "enum":                        [
+          { title: "In Progress", value: "I" },
+          { title: "On Hold", value: "H" },
+          { title: "Complete", value: "C" },
+          { title: "Deleted", value: "X" }
+        ]
       },
       "min-completion-pct": {
-        "title": "Minimum Completion Percentage",
-        "key": "minCompletion",
-        "type": "number",
-        "min": 0,
-        "max": 100,
-        "required": false
+        "title": "Minimum Completion Percentage", "key": "minCompletion",
+        "type":  "number", "min": 0, "max": 100, "required": false
       },
       "max-completion-pct": {
-        "title": "Maximum Completion Percentage",
-        "key": "maxCompletion",
-        "type": "number",
-        "min": 0,
-        "max": 100,
-        "required": false
+        "title": "Maximum Completion Percentage", "key": "maxCompletion",
+        "type":  "number", "min": 0, "max": 100, "required": false
       }
     },
-    "handler": function( req, res, next ) {
+    "handler":          function ( req, res, next ) {
+
+      if ( !req.user ) {
+        return next( Errors.HTTP_Forbidden() );
+      }
+      if ( !security["hmac-defs"]["tasker-256"].handler( req ) ) {
+        return next( Errors.HTTP_Unauthorized() );
+      }
 
       var o = {
-        _links: {},
+        tasks:     [],
+        _links:    {},
         _embedded: {}
       };
-      o._links.self = JSON.parse( JSON.stringify( getTaskListAction ) );
-      [ require( "../task/getTask" ) ].forEach( function( apiAction ) {
-        o._links[ apiAction.action ] = JSON.parse( JSON.stringify( apiAction ) );
-      } );
 
-      res.json( 200, o );
+      var assignedTo = (typeof req.query.assignedTo !== "undefined") ? req.query.assignedTo : null,
+        ownedBy = (typeof req.query.ownedBy !== "undefined") ? req.query.ownedBy : null,
+        withStatus = (typeof req.query.withStatus !== "undefined") ? req.query.withStatus : null,
+        minCompletion = (typeof req.query.minCompletion !== "undefined") ? req.query.minCompletion : 0,
+        maxCompletion = (typeof req.query.maxCompletion !== "undefined") ? req.query.maxCompletion : 100;
+
+      var dbUtil = new DBUtils( req.app.get( "client-pool" ) );
+      dbUtil.query( "SELECT * FROM table(tasker.task_mgmt.get_tasks(:1,:2,:3,:4,:5,:6))", [ assignedTo, ownedBy,
+                                                                                            withStatus, minCompletion, maxCompletion, req.user.userId ],
+                    function ( err, results ) {
+                      if ( err ) {
+                        return next( new Error( err ) );
+                      }
+                      /*if ( results.length === 0 ) {
+                        return next( Errors.HTTP_NotFound() );
+                      }*/
+
+                      results.forEach( function ( row ) {
+                        var task = new Task( row );
+                        o.tasks.push( task.id );
+                        o._embedded[task.id] = apiUtils.mergeAndClone( task, { "_links": {} } );
+                        apiUtils.generateHypermediaForAction( getTaskAction, o._embedded[task.id]["_links"], security, "self" );
+                        o._embedded[task.id]["_links"].self = apiUtils.mergeAndClone( o._embedded[task.id]["_links"].self,
+                                                                                 { "href": "/task/" + task.id,
+                                                                                   "templated": false } );
+                      } );
+
+                      apiUtils.generateHypermediaForAction( getTaskListAction, o._links, security, "self" );
+                      [ getTaskAction, require( "../auth/logout" ) ].forEach( function ( apiAction ) {
+                        apiUtils.generateHypermediaForAction( apiAction, o._links, security );
+                      } );
+                      res.json( 200, o );
+                    } );
+
     }
   };
 
