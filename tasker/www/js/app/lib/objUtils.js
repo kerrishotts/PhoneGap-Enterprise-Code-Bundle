7f1746a8d38bede27032739b694da061c612ae60
Kerri Shotts <kerrishotts@gmail.com>
2014-10-02 01:02:47 -0500

    improved merge fn, switch forEach to use Object.keys

--- tasker/www/js/app/lib/objUtils.js
+++ tasker/www/js/app/lib/objUtils.js
@@ -21,66 +21,70 @@
  * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
  */
-define ( function () {
+define( function () {
   "use strict";
   var ObjUtils = {
-    merge:       function merge()
-    {
-      var o = {};
-      var prop;
-      var args = Array.prototype.slice.call(arguments, 0);
-      args.forEach(function (arr)
-                   {
-                     for (prop in arr)
-                     {
-                       if (arr.hasOwnProperty(prop))
-                       {
-                         o[prop] = arr[prop];
-                       }
-                     }
-                   });
-      return o;
+    merge:       function merge() {
+      var t = {},
+        args = Array.prototype.slice.call( arguments, 0 );
+
+      args.forEach( function ( s ) {
+        Object.keys( s ).forEach( function ( prop ) {
+          var e = s[prop];
+          if ( typeof e === "object" && e instanceof Array ) {
+            if ( typeof t[prop] === "object" && t[prop] instanceof Array ) {
+              t[prop] = t[prop].concat( e );
+            } else if ( typeof t[prop] !== "object" || !(t[prop] instanceof Array) ) {
+              t[prop] = e;
+            }
+          } else if ( typeof e === "object" && typeof t[prop] === "object" ) {
+            t[prop] = mergeAndClone( t[prop], e );
+          } else {
+            t[prop] = e;
+          }
+        } )
+      } );
+      return t;
+      /*var o = {};
+       var prop;
+       var args = Array.prototype.slice.call( arguments, 0 );
+       args.forEach( function ( arr ) {
+       for ( prop in arr ) {
+       if ( arr.hasOwnProperty( prop ) ) {
+       o[prop] = arr[prop];
+       }
+       }
+       } );
+       return o;*/
     },
-    forEach:     function forEach(o, fn)
-    {
-      if (typeof o === "object")
-      {
-        if (typeof o.forEach !== "undefined")
-        {
-          o.forEach(fn);
+    forEach:     function forEach( o, fn ) {
+      if ( typeof o === "object" ) {
+        if ( typeof o.forEach !== "undefined" ) {
+          o.forEach( fn );
           return;
         }
       }
-      if (typeof o !== "object")
-      {
+      if ( typeof o !== "object" ) {
         return;
       }
-      if (typeof fn !== "function")
-      {
+      if ( typeof fn !== "function" ) {
         return;
       }
-      for (var prop in o)
-      {
-        if (o.hasOwnProperty(prop))
-        {
-          fn(o[prop], prop, o); // mimic value, idx, array on Array forEach
-        }
-      }
+      Object.keys( o ).forEach( function ( prop ) {
+        fn( o[prop], prop, o ); // mimic value, idx, array on Array forEach
+      } );
     },
-    interpolate: function interpolate(str, context)
-    {
+    interpolate: function interpolate( str, context ) {
       var newStr = str;
-      if (typeof context === "undefined")
-      {
+      if ( typeof context === "undefined" ) {
         return newStr;
       }
-      ObjUtils.forEach(context, function (v, prop)
-      {
-        newStr = newStr.replace("{" + prop + "}", v);
-      });
+      ObjUtils.forEach( context, function ( v, prop ) {
+        newStr = newStr.replace( "{" + prop + "}", v );
+      } );
       return newStr;
     }
   };
 
   return ObjUtils;
-});
+} );
