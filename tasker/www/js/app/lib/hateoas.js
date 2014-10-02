d924efdedc32e9b7b1657c8a60658392bd989373
Kerri Shotts <kerrishotts@gmail.com>
2014-10-02 01:02:17 -0500

    support header/body in storeResponseToContext; rename crosswalk to map

--- tasker/www/js/app/lib/hateoas.js
+++ tasker/www/js/app/lib/hateoas.js
@@ -21,57 +21,60 @@
  * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
  */
-define ( [ "app/lib/objUtils" ], function ( ObjUtils ) {
+define( [ "app/lib/objUtils" ], function ( ObjUtils ) {
   "use strict";
-  function storeResponseToContext ( r, context ) {
+  function storeResponseToContext( r, context ) {
     var selfStore;
-    if (typeof r !== "undefined" ) {
-      if (typeof r._links !== "undefined" ) {
-        if (typeof r._links.self !== "undefined" ) {
-          if (typeof r._links.self.store !== "undefined" ) {
-            selfStore = r._links.self.store;
+    if ( typeof r !== "undefined" ) {
+      if ( typeof r.body !== "undefined" ) {
+        if ( typeof r.body._links !== "undefined" ) {
+          if ( typeof r.body._links.self !== "undefined" ) {
+            if ( typeof r.body._links.self.store !== "undefined" ) {
+              selfStore = r.body._links.self.store;
+            }
           }
         }
       }
     }
-    if (selfStore === undefined) { return; }
-    ObjUtils.forEach ( selfStore, function ( v, prop ) {
-      v.forEach ( function ( item ) {
-        context[item.name] = r[item.key];
-      });
-    });
+    if ( selfStore === undefined ) { return; }
+    ObjUtils.forEach( selfStore, function ( o, oProp ) {
+      // o will be body and headers and status; we can ignore status
+      if ( typeof o === "object" ) {
+        ObjUtils.forEach( o, function ( v, prop ) {
+          v.forEach( function ( item ) {
+            context[item.name] = r[oProp][item.key];
+          } );
+        } );
+      }
+    } )
   }
 
-  function crossWalk ( o, usingTemplate ) {
+  function map( o, usingTemplate ) {
     var newO = {};
-    ObjUtils.forEach ( usingTemplate, function ( v, prop ) {
+    ObjUtils.forEach( usingTemplate, function ( v, prop ) {
       newO[v.key] = o[prop];
-    });
+    } );
     return newO;
   }
 
-
-  function buildHeadersAttachment ( headers, context ) {
+  function buildHeadersAttachment( headers, context ) {
     var returnHeaders = [];
-    if (typeof headers === "undefined") { return returnHeaders; }
-    headers.forEach ( function ( header ) {
-      if (typeof header.templated === "undefined" ||
-          !header.templated)
-      {
-        returnHeaders.push({ headerName:  header.key,
-                             headerValue: header.value });
-      } else
-      {
-        returnHeaders.push({ headerName:  header.key,
-                             headerValue: ObjUtils.interpolate(header.value, context)});
+    if ( typeof headers === "undefined" ) { return returnHeaders; }
+    headers.forEach( function ( header ) {
+      if ( typeof header.templated === "undefined" || !header.templated ) {
+        returnHeaders.push( { headerName:  header.key,
+                              headerValue: header.value } );
+      } else {
+        returnHeaders.push( { headerName:  header.key,
+                              headerValue: ObjUtils.interpolate( header.value, context )} );
       }
-    });
+    } );
     return returnHeaders;
   }
 
   return {
     storeResponseToContext: storeResponseToContext,
-    crossWalk: crossWalk,
+    map:                    map,
     buildHeadersAttachment: buildHeadersAttachment
   }
-});
+} );
