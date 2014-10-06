ce055119a6c0262dc4efc74eff9cd8e3ec6b61e4
Kerri Shotts <kerrishotts@gmail.com>
2014-10-05 20:45:58 -0500

    revised chain

--- ch5/www/js/app/main.js
+++ ch5/www/js/app/main.js
@@ -20,166 +20,180 @@
  * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
  */
-/*jshint
-         asi:true,
-         bitwise:true,
-         browser:true,
-         curly:true,
-         eqeqeq:false,
-         forin:true,
-         noarg:true,
-         noempty:true,
-         plusplus:false,
-         smarttabs:true,
-         sub:true,
-         trailing:false,
-         undef:true,
-         white:false,
-         onevar:false 
- */
 /*global define*/
 define( [ "yasmf", "app/lib/xhr",
-                   "app/lib/objUtils",
-                   "app/lib/hateoas",
-                   "app/models/session",
-                   "socket.io"], function( _y, XHR, ObjUtils, Hateoas, Session, io ) {
+  "app/lib/objUtils",
+  "app/lib/hateoas",
+  "app/models/session",
+  "socket.io", "app/lib/cryptojs"
+], function ( _y, XHR, ObjUtils, Hateoas, Session, io, CryptoJS ) {
   "use strict";
   // private methods
   // syntax highlight & pretty print from http://stackoverflow.com/a/7220510/741043
-  function syntaxHighlight(json) {
-    if (typeof json != 'string') {
-      json = JSON.stringify(json, undefined, 4);
+  function syntaxHighlight( json ) {
+    if ( typeof json != 'string' ) {
+      json = JSON.stringify( json, undefined, 4 );
     }
-    try
-    {
-      json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-      return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match)
-      {
-        var cls = 'number';
-        if (/^"/.test(match))
-        {
-          if (/:$/.test(match))
-          {
-            cls = 'key';
-          } else
-          {
-            cls = 'string';
+    try {
+      json = json.replace( /&/g, '&amp;' )
+        .replace( /</g, '&lt;' )
+        .replace( />/g, '&gt;' );
+      return json.replace(
+        /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (
+          match ) {
+          var cls = 'number';
+          if ( /^"/.test( match ) ) {
+            if ( /:$/.test( match ) ) {
+              cls = 'key';
+            } else {
+              cls = 'string';
+            }
+          } else if ( /true|false/.test( match ) ) {
+            cls = 'boolean';
+          } else if ( /null/.test( match ) ) {
+            cls = 'null';
           }
-        } else if (/true|false/.test(match))
-        {
-          cls = 'boolean';
-        } else if (/null/.test(match))
-        {
-          cls = 'null';
-        }
-        return '<span class="' + cls + '">' + match + '</span>';
-      });
-    } catch (err) {
+          return '<span class="' + cls + '">' + match + '</span>';
+        } );
+    } catch ( err ) {
       return json;
     }
   }
 
-  function log ( msg ) {
+  function log( msg ) {
     var logMsg = msg,
       console;
-    if (typeof msg !== "string") {
-      logMsg = syntaxHighlight (msg);
+    if ( typeof msg !== "string" ) {
+      logMsg = syntaxHighlight( msg );
     }
-    var e = document.createElement("div");
-    e.innerHTML = "Logged on: " + (new Date()).toString();
-    var pre = document.createElement("pre");
+    var e = document.createElement( "div" );
+    e.innerHTML = "Logged on: " + ( new Date() )
+      .toString();
+    var pre = document.createElement( "pre" );
     pre.innerHTML = logMsg;
-    e.appendChild(pre);
+    e.appendChild( pre );
 
-    console = document.getElementById ("console");
-    console.insertBefore (e, console.firstChild);
+    console = document.getElementById( "console" );
+    console.insertBefore( e, console.firstChild );
     //.appendChild (e);
   }
 
-  function logMore () {
-    for (var i = 0; i<arguments.length; i++) {
-      log ( arguments[i] );
+  function logMore() {
+    for ( var i = 0; i < arguments.length; i++ ) {
+      log( arguments[ i ] );
     }
+  }
+
+  function makeHMAC( url, body, context ) {
+    function pad2( v ) {
+      return ( v < 10 ) ? "0" + v : "" + v;
+    }
+    var now = new Date(),
+      nowYYYY,
+      nowMM,
+      nowDD,
+      nowHH,
+      nowMI,
+      dateString = "",
+      stringToHmac = "",
+      hmacString = "";
+    now.setMinutes( now.getMinutes() );
+    nowYYYY = now.getUTCFullYear();
+    nowMM = now.getUTCMonth() + 1;
+    nowDD = now.getUTCDate();
+    nowHH = now.getUTCHours();
+    nowMI = now.getUTCMinutes();
+    dateString = "" + nowYYYY + pad2( nowMM ) + pad2( nowDD ) + "." + pad2( nowHH ) + pad2( nowMI );
+    stringToHmac = "" + dateString + "." + url;
+    if ( body !== undefined && body !== null ) {
+      stringToHmac += JSON.stringify( body );
+    }
+    hmacString = CryptoJS.HmacSHA256( stringToHmac, context[ "hmac-token" ] )
+      .toString( CryptoJS.enc.Hex );
+
+    context[ "hmac-token" ] = hmacString;
+
   }
 
   // define our app object
   var APP = {};
-  APP.start = function() {
-    var baseURI = "https://pge-as.acmecorp.com:4443";
+  APP.start = function () {
+    var baseURI = "https://localhost:4443";
     var session;
     var context = {};
 
     console.log = logMore;
 
     // to be really secure, this should be checked before EVERY XHR. For the demo, once is sufficient.
-    XHR.checkIfSecure ( baseURI, ["27 02 A5 EB 95 91 41 66 C3 9F 82 D3 59 14 13 0E 13 B5 13 9E"])
-      .then ( function ( msg ) {
-      // example straight from http://socket.io/docs/
-      var socket = io('https://pge-as.acmecorp.com:4443');
-      socket.on('news', function (data) {
-        console.log(data);
-        socket.emit('my other event', { my: 'data' });
-      });
+    XHR.checkIfSecure( baseURI, [ "27 02 A5 EB 95 91 41 66 C3 9F 82 D3 59 14 13 0E 13 B5 13 9E" ] )
+      .then( function ( msg ) {
+        // example straight from http://socket.io/docs/
+        var socket = io( 'https://pge-as.acmecorp.com:4443' );
+        socket.on( 'news', function ( data ) {
+          console.log( data );
+          socket.emit( 'my other event', {
+            my: 'data'
+          } );
+        } );
 
-      return XHR.send ( "GET", baseURI + "/");
-    })
-      .then ( function ( r ) {
-      console.log ("Response from API Discovery", r);
-      return XHR.send (r._links["get-token"].verb, baseURI + r._links["get-token"].href );
-    })
-      .then ( function ( r) {
-      console.log ("Response from CSRF token request", r);
+        return XHR.send( "GET", baseURI + "/" );
+      } )
+      .then( function ( r ) {
+        console.log( "Response from API Discovery", r );
+        return XHR.send( r.body._links[ "get-csrf-token" ].verb, baseURI + r.body._links[ "get-csrf-token" ].href );
+      } )
+      .then( function ( r ) {
+        console.log( "Response from CSRF token request", r );
 
-      Hateoas.storeResponseToContext( r, context );
+        Hateoas.storeResponseToContext( r, context );
 
-      // create post response based upon the template
-      var loginAction = r._links["login"];
-      var postData = Hateoas.crossWalk ({
-                                        "user-id":            "JDOE",
-                                        "candidate-password": "password"
-                                      }, loginAction.template );
+        // create post response based upon the template
+        var loginAction = r.body._links[ "login" ];
+        var postData = Hateoas.map( {
+          "user-id": "JDOE",
+          "candidate-password": "password"
+        }, loginAction.template );
 
-      // build response headers
-      var headers = Hateoas.buildHeadersAttachment( loginAction.attachments.headers, context );
+        // build response headers
+        var headers = Hateoas.buildHeadersAttachment( loginAction.attachments.headers, context );
 
-      // send request
-      return XHR.send ( r._links["login"].verb, baseURI + r._links["login"].href, { data: postData, headers: headers } );
-    }).then ( function ( r ) {
-      console.log (r, "Response from AUTH POST");
-      Hateoas.storeResponseToContext ( r, context );
-      console.log ( context, "Context after storing" );
-      session = new Session ( { sessionId: context["session-id"],
-                                sessionSalt: context["session-salt"],
-                                userId: context["user-id"],
-                                nextIntermediateToken: context["next-token"]
-                              } );
-      console.log ( session, "Session after pulling from context" );
-      context["next-token"] = session.computeNextToken();
+        // send request
+        return XHR.send( r.body._links[ "login" ].verb, baseURI + r.body._links[ "login" ].href, {
+          data: postData,
+          headers: headers
+        } );
+      } )
+      .then( function ( r ) {
+        console.log( r, "Response from AUTH POST" );
+        Hateoas.storeResponseToContext( r, context );
+        console.log( context, "Context after storing" );
+        session = new Session( {
+          sessionId: context[ "session-id" ],
+          hmacSecret: context[ "hmac-secret" ],
+          userId: context[ "user-id" ],
+          nextToken: context[ "next-token" ]
+        } );
+        console.log( session, "Session after pulling from context" );
 
-      var headers = Hateoas.buildHeadersAttachment ( r._links["get-task"].attachments.headers, context );
+        var URI = ObjUtils.interpolate( r.body._links[ "get-task" ].href, {
+          "taskId": 2
+        } );
 
-      var URI = baseURI + ObjUtils.interpolate (r._links["get-task"].href, { "taskId": 2 } );
+        makeHMAC( URI, undefined, context );
 
-      return XHR.send ( r._links["get-task"].verb, URI, { headers: headers });
-    }).then ( function ( r ) {
-      console.log ( r, "Response from GET task");
-      Hateoas.storeResponseToContext( r, context );
-      session.nextIntermediateToken = context["next-token"];
-      context["next-token"] = session.computeNextToken();
+        var headers = Hateoas.buildHeadersAttachment( r.body._links[ "get-task" ].attachments.headers, context );
 
-      var headers = Hateoas.buildHeadersAttachment ( r._links["self"].attachments.headers, context );
-
-      var URI = baseURI + ObjUtils.interpolate (r._links["self"].href, { "taskId": 2 } );
-
-      return XHR.send ( r._links["self"].verb, URI, { headers: headers });
-    }).then ( function ( r ) {
-      console.log ( r, "response from GET self" );
-      Hateoas.storeResponseToContext( r, context );
-      session.nextIntermediateToken = context["next-token"];
-      context["next-token"] = session.computeNextToken();
-    }).catch ( function ( err ) {
-      console.log ( err, "failure in chain");
-    }).done();
+        return XHR.send( r.body._links[ "get-task" ].verb, baseURI + URI, {
+          headers: headers
+        } );
+      } )
+      .then( function ( r ) {
+        console.log( r, "Response from GET task" );
+      } )
+      .catch( function ( err ) {
+        console.log( err, "failure in chain" );
+      } )
+      .done();
 
 
   };
