138c13df4ca0aa0a6c7ae94e9ef83cb6877e00fb
Kerri Shotts <kerrishotts@gmail.com>
2014-10-05 20:46:07 -0500

    revised session

--- ch5/www/js/app/models/session.js
+++ ch5/www/js/app/models/session.js
@@ -1,20 +1,20 @@
-define ( [ "app/lib/objUtils", "../lib/cryptojs" ], function ( objUtils, CryptoJS) {
-  function Session ( data ) {
-    if (typeof data !== "undefined") {
+define( function () {
+  "use strict";
+
+  function Session( data ) {
+    if ( data !== undefined ) {
       this.sessionId = data.sessionId;
       this.userId = data.userId;
-      this.sessionSalt = data.sessionSalt;
-      this.nextIntermediateToken = data.nextIntermediateToken;
+      this.hmacSecret = data.hmacSecret;
+      this.nextToken = data.nextToken;
     }
   }
 
-  Session.prototype.computeNextToken = function () {
-    var token1 = this.nextIntermediateToken.substr(0,256);
-    var token2 = this.nextIntermediateToken.substr(256,256);
-    var token2H = CryptoJS.PBKDF2( token2, this.sessionSalt,
-                                   {keySize: 32, iterations: 256}).toString(CryptoJS.enc.Hex).toUpperCase();
-    return token1+token2H;
+  Session.prototype.setNextToken = function setNextToken( nextToken ) {
+    if ( nextToken !== undefined && nextToken !== null ) {
+      this.nextToken = nextToken;
+    }
   };
 
   return Session;
-});
+} );
