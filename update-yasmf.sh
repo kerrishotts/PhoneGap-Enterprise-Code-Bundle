9af18abf7ee2f33fe26b95538087d6dc2be55594
Kerri Shotts <kerrishotts@gmail.com>
2014-12-11 01:41:00 -0600

    yasmf now has multiple js files

--- update-yasmf.sh
+++ update-yasmf.sh
@@ -7,7 +7,7 @@ echo "Finding all subdirectories..."
 for aDir in `find . -name www -noleaf -maxdepth 3 -type d`; do
 echo "... Updating $aDir";
 echo "... Copying yasmf.js to ./$aDir/js/lib";
-cp ~/Documents/Mobile/YASMF/YASMF-Next/dist/yasmf.js "./$aDir/js/lib"
+cp ~/Documents/Mobile/YASMF/YASMF-Next/dist/*.js "./$aDir/js/lib"
 echo "... Copying yasmf.css ./$aDir/js/lib";
 cp ~/Documents/Mobile/YASMF/YASMF-Next/dist/yasmf.css "./$aDir/js/lib"
 echo "... Copying yasmf assets ./$aDir/js/lib";
